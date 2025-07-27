1db6c1129ee974661f1d7bf15a04e65c3f0393d0
lml <2311067@mail.nankai.edu.cn>
2025-07-27 14:38:26 +0800

    usr args

--- auto_run_eval.py
+++ auto_run_eval.py
@@ -2,6 +2,7 @@ from tqdm import tqdm
 from datetime import datetime
 import subprocess
 import json
+import argparse
 
 
 def timestamp():
@@ -22,103 +23,141 @@ def print_summary(finished_tasks):
     print()
 
 
-# load tasks
-with open("tasks.json", "r") as f:
-    config = json.load(f)
-
-tasks = config["tasks"]
-skip = config.get("skip", [])
-primary = config.get("primary", [])
-gpu = 1
-ckpt_name = "last.ckpt"
-batch_size = 6
-gt_path = "../DataStore/WHU_512_small"
-ddpm_step = 200
-ddim_step = 200
-eta = 0.5
-save_path = "eval_results_new.csv"
-
-success_tasks = []
-failed_tasks = []
-
-
-pbar = tqdm(total=len(tasks), desc="Total Progress", dynamic_ncols=True)
-finished_tasks = []
-while tasks:
-    if primary:
-        task_id = str(primary.pop(0))
-        task = tasks.pop(task_id)
-    else:
-        task_id, task = tasks.popitem()
-    print(f"\033[94m[{timestamp()}] Evaluating task {int(task_id)}\033[0m")
-    # print(f"evaluating task {int(task_id)}")
-
-    mode = task["mode"]
-    logdir = task["logdir"]
-    script = task["script"]
-    step_flag = task["step_flag"]
-    step_value = task["step_value"]
-    dataset = task["dataset"]
-
-    # === 跳过逻辑（例）===
-    if int(task_id) in skip:
-        # print(f"Skiping task {int(task_id)}")
-        print(f"\033[94mSkipping task {int(task_id)}\033[0m")
-        continue
-
-    cmd = [
-        "python",
-        script,
-        "--logdir",
-        logdir,
-        "--gpu",
-        str(gpu),
-        "--ckpt_name",
-        ckpt_name,
-        "--batch_size",
-        str(batch_size),
-        "--gt_path",
-        gt_path,
-        step_flag,
-        str(step_value),
+if __name__ == "__main__":
+    # add args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--task_path", type=str, help="Path of the json file that records tasks"
+    )
+    parser.add_argument(
+        "--ckpt_name", type=str, help="The name of ckpt that you want to eval"
+    )
+    parser.add_argument("--gt_path", type=str, help="Path to test set image dir")
+    parser.add_argument("--gpu", type=int, help="GPU id")
+    parser.add_argument(
+        "--batch_size", type=int, help="batch_size to use ,default 6", default=6
+    )
+    parser.add_argument(
         "--save_path",
-        save_path,
+        type=str,
+        help="Path to csv file that records final result of each task",
+    )
+    parser.add_argument(
         "--detail_dir",
-        "eval_unet_new"
-    ]
+        type=str,
+        help="Path to the dir that saves detail results of this run of tasks",
+    )
+    parser.add_argument(
+        "--ddpm_step", type=int, help="DDPM steps when valing each image", default=200
+    )
+    parser.add_argument(
+        "--ddim_step", type=int, help="DDIM steps when valing each image", default=200
+    )
+    parser.add_argument(
+        "--eta", type=float, help="ETA to use when in DDIM mode", default=0.5
+    )
 
-    if "ddim" in script:
-        cmd += ["--ddim_eta", str(eta)]
-        cmd += ["--use_ddim"]
+    args = parser.parse_args()
 
-    if dataset:
-        cmd += ["--dataset", dataset]
+    # load tasks
+    task_path = args.task_path
+    with open(task_path, "r") as f:
+        config = json.load(f)
 
-    try:
-        subprocess.run(cmd, check=True)
-        msg = f"[{task_id}] {mode} success: {logdir}"
-        success_tasks.append(msg)
-    except Exception as e:
-        msg = f"[{task_id}] {mode} failed: {logdir}"
-        failed_tasks.append(msg)
-    finished_tasks.append(int(task_id))
-    finished_tasks.sort()
-    pbar.update(1)
-    print_summary(finished_tasks)
+    tasks = config["tasks"]
+    skip = config.get("skip", [])
+    primary = config.get("primary", [])
 
-pbar.close()
+    gpu = args.gpu
+    ckpt_name = args.ckpt_name
+    batch_size = args.batch_size
+    gt_path = args.gt_path
+    ddpm_step = args.ddpm_step
+    ddim_step = args.ddim_step
+    eta = args.eta
+    save_path = args.save_path
+    detail_dir = args.detail_dir
 
-# === 汇总 ===
-print(f"\n\033[1m[{timestamp()}] \033[94mFINAL SUMMARY\033[0m")
+    success_tasks = []
+    failed_tasks = []
 
-if success_tasks:
-    print("\033[92m[Succeeded:]\033[0m")
-    for s in success_tasks:
-        print(f"\033[92m✔ {s}\033[0m")
+    pbar = tqdm(total=len(tasks), desc="Total Progress", dynamic_ncols=True)
+    finished_tasks = []
+    while tasks:
+        if primary:
+            task_id = str(primary.pop(0))
+            task = tasks.pop(task_id)
+        else:
+            task_id, task = tasks.popitem()
+        print(f"\033[94m[{timestamp()}] Evaluating task {int(task_id)}\033[0m")
+        # print(f"evaluating task {int(task_id)}")
 
-if failed_tasks:
-    print("\033[91m[Failed:]\033[0m")
-    for f in failed_tasks:
-        print(f"\033[91m✘ {f}\033[0m")
-else:
-    print("\033[92mAll evaluations completed successfully!\033[0m")
+        mode = task["mode"]
+        logdir = task["logdir"]
+        script = task["script"]
+        step_flag = task["step_flag"]
+        step_value = task["step_value"]
+        dataset = task["dataset"]
+
+        # === 跳过逻辑（例）===
+        if int(task_id) in skip:
+            # print(f"Skiping task {int(task_id)}")
+            print(f"\033[94mSkipping task {int(task_id)}\033[0m")
+            continue
+
+        cmd = [
+            "python",
+            script,
+            "--logdir",
+            logdir,
+            "--gpu",
+            str(gpu),
+            "--ckpt_name",
+            ckpt_name,
+            "--batch_size",
+            str(batch_size),
+            "--gt_path",
+            gt_path,
+            step_flag,
+            str(step_value),
+            "--save_path",
+            save_path,
+            "--detail_dir",
+            detail_dir,
+        ]
+
+        if "ddim" in script:
+            cmd += ["--ddim_eta", str(eta)]
+            cmd += ["--use_ddim"]
+
+        if dataset:
+            cmd += ["--dataset", dataset]
+
+        try:
+            subprocess.run(cmd, check=True)
+            msg = f"[{task_id}] {mode} success: {logdir}"
+            success_tasks.append(msg)
+        except Exception as e:
+            msg = f"[{task_id}] {mode} failed: {logdir}"
+            failed_tasks.append(msg)
+        finished_tasks.append(int(task_id))
+        finished_tasks.sort()
+        pbar.update(1)
+        print_summary(finished_tasks)
+
+    pbar.close()
+
+    # === 汇总 ===
+    print(f"\n\033[1m[{timestamp()}] \033[94mFINAL SUMMARY\033[0m")
+
+    if success_tasks:
+        print("\033[92m[Succeeded:]\033[0m")
+        for s in success_tasks:
+            print(f"\033[92m✔ {s}\033[0m")
+
+    if failed_tasks:
+        print("\033[91m[Failed:]\033[0m")
+        for f in failed_tasks:
+            print(f"\033[91m✘ {f}\033[0m")
+    else:
+        print("\033[92mAll evaluations completed successfully!\033[0m")
