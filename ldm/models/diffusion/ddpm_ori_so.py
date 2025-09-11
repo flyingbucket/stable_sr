9d775646f6ae8ac5e764522afe8c75982a8cd129
lml <2311067@mail.nankai.edu.cn>
2025-09-11 12:00:07 +0800

    fix:quantize error

--- ldm/models/diffusion/ddpm_ori_so.py
+++ ldm/models/diffusion/ddpm_ori_so.py
@@ -623,8 +623,8 @@ class LatentDiffusionOriSO(LatentDiffusion):
 
         if clip_denoised:
             x_recon.clamp_(-1.0, 1.0)
-        if quantize_denoised:
-            x_recon, _, [_, _, indices] = self.first_stage_model.quantize(x_recon)
+        # if quantize_denoised:
+        #     x_recon, _, [_, _, indices] = self.first_stage_model.quantize(x_recon)
 
         model_mean, posterior_variance, posterior_log_variance = self.q_posterior(
             x_start=x_recon, x_t=x, t=t
